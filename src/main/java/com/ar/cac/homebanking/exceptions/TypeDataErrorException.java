d360477f18d23d1d027dbc7047b04b67395e2564
tinchoplayero <martin.domanski@outlook.com>
2023-12-05 10:27:16 -0300

    Revert "Revert "Excepciones personalizadas en Transferencias""
    This reverts commit 5ebbfc91d1d66f2266d0fd394396461d30204f48.

---
+++ src/main/java/com/ar/cac/homebanking/exceptions/TypeDataErrorException.java
@@ -0,0 +1,8 @@
+package com.ar.cac.homebanking.exceptions;
+
+
+public class TypeDataErrorException extends RuntimeException {
+    public TypeDataErrorException(String message) {
+        super(message);
+    }
+}
